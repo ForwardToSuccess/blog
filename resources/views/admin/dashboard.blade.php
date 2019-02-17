7d68ffdbe61222f9252707ed73b5a89ff4217545
ForwardToSuccess <m.l.forwardtosuccess@gmail.com>
2019-02-17 11:43:14 +0200

    Добавлена административная страница

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Максим
- * Date: 16.02.2019
- * Time: 23:14
- */
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Категорий 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Материалов 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Посетителей 0</span></p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p><span class="label label-primary">Пос. сегодня 0</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a href="#" class="btn btn-block btn-default">Создать категорию</a>
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Категория первая</h4>
+                    <p class="list-group-item-text">
+                        Кол-во материалов
+                    </p>
+                </a></div>
+            <div class="col-sm-6">
+                <a href="#" class="btn btn-block btn-default">Создать Материал</a>
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Материал первый</h4>
+                    <p class="list-group-item-text">
+                        Категория
+                    </p>
+                </a></div>
+        </div>
+    </div>
+
+@endsection
